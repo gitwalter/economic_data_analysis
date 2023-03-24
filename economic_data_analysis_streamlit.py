0de8dd726e485d3790b028875ac5da348e9214b1
gitwalter <w.pogantsch@googlemail.com>
2023-03-24 18:44:22 +0100

    reset session state

--- economic_data_analysis_streamlit.py
+++ economic_data_analysis_streamlit.py
@@ -210,6 +210,7 @@ class EconomicDataAnalysis:
                 # reset session state
                 st.session_state.fetched_countries = []
                 st.session_state.fetched_indicators = []
+                st.session_state.df_wb_indicators_countries = pd.DataFrame()
                 st.header('error fetching data at worldbank for', indicators, countries)
                 st.write(err)
                 return
@@ -254,7 +255,7 @@ class EconomicDataAnalysis:
                         try:
                             self.df_indicator_per_country[country_name] = df_indicator.loc[country_name]
                         except:
-                            st.write('no data for ', indicator_name, country_name)
+                            st.write('No data for ', indicator_name, country_name)
 
                 self.plotting()   
     
